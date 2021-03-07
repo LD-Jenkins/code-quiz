5826e595814368dd63b35bf325e66282b4e42e25
Leighton Jenkins <leightonjenkins84@gmail.com>
2021-03-06 23:53:38 -0600

    finalized html, ccs, js, added readme content

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # code-quiz
+
+This project is now live at https://ld-jenkins.github.io/code-quiz/ !
+
+## Project Description
+
+Have you ever wanted to test your basic HTML, CSS and Javascript skillz? Look no further.
+
+My Code Quiz™ will test your ball of pink meat nestled within the cavity doctors would call a "skull" to see if you have what it takes to be the world's next Big Ol' Heckin' Genius of 2012™.
+
+The only thing now between you and glory is clicking "BEGIN".
+
+## The Rules
+
+Once you've clicked begin you will immediately be prompted with a question and a countdown from one minute will commence. Any question answered correctly will give you one point while any question answered incorrectly will not give you one point and subtract five seconds from the timer.
+
+Once the timer reaches zero or you have made it through all of the available questions, whichever happens first, you will be able to see the results of the knowledge you have gained over the years and finally show that it was totally worth that semester at community college.
+
+If you enjoy keeping records of your brilliance you can even save your score along with your initials so you can show all of your friends and extended family at the next community potluck.
+
+![First Page](./assets/images/firstpage.PNG)
+
+![Second Page](./assets/images/secondpage.PNG)
+
+![Results!](./assets/images/resultspage.PNG)
+
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
-var beginButton = document.querySelector("button");
+var beginButton = document.getElementById("begin-button");
 var qDiv = document.getElementById("questions");
-var divWrapper = document.getElementById("wrapper");
+var divWrapper = document.getElementById("main-wrapper");
+var rDiv = document.getElementById("results");
 var timeDisplay = document.getElementById("time");
+
 
 var iter = 0;
 var score = 0;
@@ -9,6 +11,12 @@ var time = 59;
 
 var beginWasClicked = false;
 
+var userSubmission = {
+    initials: "",
+    score: 0
+}
+
+var subArray = [];
 
 var question1 = {
     q: "In Javascript, what is it called when you create a new variable and give it a value on the same line.",
@@ -28,11 +36,126 @@ var question2 = {
     correct: "opt3"
 }
 
-var qArray = [question1, question2];
+var qArray = [
+    
+    {
+        q: "In Javascript, what is it called when you create a new variable and give it a value on the same line?",
+        one: "Initialization",
+        two: "Z-Index",
+        three: "Hoisting",
+        four: "Nullify",
+        correct: "opt1"
+    }, 
+
+    {
+        q: "What is the default flex-direction for an element with display: flex?",
+        one: "Column",
+        two: "Row-Reverse",
+        three: "Row",
+        four: "Diagonal",
+        correct: "opt3"
+    }, 
+
+    {
+        q: "Which of the following would give an element 3px of left and right padding, 5px top padding, and 10px bottom padding?",
+        one: "3px 3px 5px 10px",
+        two: "5px 3px 10px 3px",
+        three: "3px 5px 10px 3px",
+        four: "Tomato Soup",
+        correct: "opt2"
+    },
+
+    {
+        q: "Which of the following evaluates as falsey?",
+        one: "[] (an empty array)",
+        two: "{} (an empty object)",
+        three: "function foo() {}",
+        four: '"" (an empty string)',
+        correct: "opt4"
+    },
+
+    {
+        q: "Which element is not a block level element?",
+        one: "<img>",
+        two: "<div>",
+        three: "<figure>",
+        four: "<p>",
+        correct: "opt1"
+    },
+
+    {
+        q: "What browser storage persists between sessions?",
+        one: "LocalStorage",
+        two: "SessionStorage",
+        three: "TheBoot",
+        four: "AsyncStorage",
+        correct: "opt1"
+    },
+
+    {
+        q: "Which selector would select all p elements inside a div with a class name of 'foo'",
+        one: "div.foo p",
+        two: "div#foo p::all",
+        three: "#foo #div #p",
+        four: ".foo dive p.all",
+        correct: "opt1"
+    },
+
+];
 
 var timer = setInterval(updateTime, 1000);
 
+function renderAttempts() {
+
+    var stringToParse = localStorage.getItem("attempts") || '[]';
+    var parsedArray = JSON.parse(stringToParse);
+
+    var attemptsList = document.getElementById("attempt-list");
+    attemptsList.innerHTML = "";
+
+    for (var i = 0; i < parsedArray.length; i++){
+        var listItem = document.createElement("li");
+        listItem.textContent = parsedArray[i].initials + ": " + parsedArray[i].score;
+        attemptsList.appendChild(listItem);
+    }
+}
+
+function saveScore() {
+
+    var userInitials = document.getElementById("initials").value;
+    userSubmission.initials = userInitials;
+    userSubmission.score = score;
+
+    var stringToParse = localStorage.getItem("attempts");
+
+    if (stringToParse === null) {
+        subString = JSON.stringify([userSubmission]);
+        localStorage.setItem("attempts", subString);
+    } else {
+        var parsedArray = JSON.parse(stringToParse);
+        parsedArray.push(userSubmission);
+        subArray = parsedArray;
+        var stringifiedArray = JSON.stringify(parsedArray);
+        localStorage.setItem("attempts", stringifiedArray);
+    }
+
+    renderAttempts();
+
+}
+
 function endGameNoSpoilers() {
+    
+    clearDiv();
+    divWrapper.innerHTML = rDiv.innerHTML;
+
+    renderAttempts();
+
+    var scoreDisplay = document.getElementById("score-display");
+    scoreDisplay.textContent = score;
+
+    var submitButton = document.getElementById("submit-button");
+
+    submitButton.addEventListener("click", saveScore, {once: true})
     
 }
 
@@ -59,15 +182,16 @@ function checkCorrect(event) {
         time -= 5;
     }
     
+    iter++;
     if (iter == qArray.length) {
+
         clearInterval(timer);
         endGameNoSpoilers();  
     } else {
+        
         displayQuestion();
     }
     
-
-    iter++;
 }
 
 function clearDiv() {
